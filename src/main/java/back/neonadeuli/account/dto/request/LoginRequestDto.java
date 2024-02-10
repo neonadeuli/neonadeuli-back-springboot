3a96da6f103649ed16acc6bf602abb759e851b0f
eora21 <eora21@naver.com>
2024-02-10 16:34:02 +0900

    feat: 🎸 로그인 시 요구될 dto 작성

---
+++ src/main/java/back/neonadeuli/account/dto/request/LoginRequestDto.java
@@ -0,0 +1,22 @@
+package back.neonadeuli.account.dto.request;
+
+import static back.neonadeuli.account.entity.Account.LOGIN_ID_MAX_SIZE;
+import static back.neonadeuli.account.entity.Account.LOGIN_ID_MIN_SIZE;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LoginRequestDto {
+
+    @NotEmpty
+    @Size(min = LOGIN_ID_MIN_SIZE, max = LOGIN_ID_MAX_SIZE)
+    private String loginId;
+
+    @NotEmpty
+    private String password;
+}
