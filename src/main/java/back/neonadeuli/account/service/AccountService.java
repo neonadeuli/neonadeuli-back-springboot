5774afad1131cc135bb63fbeb609c50aeb5bc5f4
eora21 <eora21@naver.com>
2024-02-10 16:38:49 +0900

    feat: 🎸 Account Service 작성

---
+++ src/main/java/back/neonadeuli/account/service/AccountService.java
@@ -0,0 +1,36 @@
+package back.neonadeuli.account.service;
+
+import back.neonadeuli.account.dto.request.LoginRequestDto;
+import back.neonadeuli.account.dto.request.SignupRequestDto;
+import back.neonadeuli.account.dto.response.LoginResponseDto;
+import back.neonadeuli.account.dto.response.SignupResponseDto;
+import back.neonadeuli.account.entity.Account;
+import back.neonadeuli.account.exception.LoginFailureException;
+import back.neonadeuli.account.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SignupResponseDto signup(SignupRequestDto requestDto) {
+        Account saveAccount = accountRepository.save(requestDto.toEntity(passwordEncoder));
+        return new SignupResponseDto(saveAccount.getId());
+    }
+
+    public LoginResponseDto login(LoginRequestDto requestDto) {
+        Account loginAccount = accountRepository.findByLoginId(requestDto.getLoginId())
+                .orElseThrow(LoginFailureException::new);
+
+        if (passwordEncoder.matches(requestDto.getPassword(), loginAccount.getPassword())) {
+            return new LoginResponseDto(loginAccount.getId());
+        }
+
+        throw new LoginFailureException();
+    }
+}
