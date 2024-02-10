fa846d88d762d552314d0c43fb59a90c7297aacf
eora21 <eora21@naver.com>
2024-02-10 16:26:44 +0900

    refactor: 💡 이미지 생성자 추가, 기본 이미지 작성
    이미지 부분은 어떠한 storage를 사용할지, 어떤 기본 이미지를 사용할지 정해지지 않았으므로 단순 회원 가입용 데이터를
    작성했습니다.

--- src/main/java/back/neonadeuli/picture/entity/Picture.java
+++ src/main/java/back/neonadeuli/picture/entity/Picture.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,7 +16,10 @@ import lombok.NoArgsConstructor;
 @Table(name = "Pictures")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Picture {
+
+    public static Picture DEFAULT_PICTURE = new Picture(1L, "", "", "");
 
     @Id
     @Column(name = "picture_id")
@@ -33,4 +37,10 @@ public class Picture {
     @Column
     @NotNull
     private String savedName;
+
+    public Picture(String folderName, String originName, String savedName) {
+        this.folderName = folderName;
+        this.originName = originName;
+        this.savedName = savedName;
+    }
 }
