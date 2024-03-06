1aa5e7c13b01e470a4d60141b13e7d7beb097ee3
eora21 <eora21@naver.com>
2024-03-06 15:47:02 +0900

    fix: 🐛 커스텀 예외를 제외한 나머지 예외를 스프링에게 전담하도록 수정

--- src/main/java/back/neonadeuli/exception/DefaultExceptionHandler.java
+++ src/main/java/back/neonadeuli/exception/DefaultExceptionHandler.java
@@ -3,8 +3,6 @@ package back.neonadeuli.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,18 +14,5 @@ public class DefaultExceptionHandler {
     ResponseEntity<String> handleCustomException(CustomException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(e.getMessage());
-    }
-
-    @ExceptionHandler({HttpMessageNotReadableException.class, HttpRequestMethodNotSupportedException.class})
-    ResponseEntity<String> handleMistakeException(Exception e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(e.getMessage());
-    }
-
-    @ExceptionHandler(Exception.class)
-    ResponseEntity<String> handleException(Exception e) {
-        log.error("{} : {}", e, e.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
     }
 }
