5e99dfa6a627ee79ba2ad3c0a71dbbcb2b57534a
eora21 <eora21@naver.com>
2024-03-07 20:11:08 +0900

    refactor: 💡 커스텀 예외에 특정 예외 코드 담을 수 있도록 작성

--- src/main/java/back/neonadeuli/exception/CustomException.java
+++ src/main/java/back/neonadeuli/exception/CustomException.java
@@ -1,8 +1,19 @@
 package back.neonadeuli.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public abstract class CustomException extends RuntimeException {
 
-    protected CustomException(String message) {
+    private final HttpStatus httpStatus;
+
+    protected CustomException(String message, HttpStatus httpStatus) {
         super(message);
+        this.httpStatus = httpStatus;
+    }
+
+    protected CustomException(String message) {
+        this(message, HttpStatus.BAD_REQUEST);
     }
 }
