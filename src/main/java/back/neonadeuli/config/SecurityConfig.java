73f8a64c3d11961b9453dedd634185875b0cadfa
eora21 <eora21@naver.com>
2024-03-07 20:50:11 +0900

    feat: 🎸 스프링 시큐리티 세션 생성 금지, 사용은 승낙

--- src/main/java/back/neonadeuli/config/SecurityConfig.java
+++ src/main/java/back/neonadeuli/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,6 +23,8 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable);
 
         http.formLogin(AbstractHttpConfigurer::disable);
+        
+        http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.NEVER));
 
         http.authorizeHttpRequests(authorizeRequests -> {
             authorizeRequests.requestMatchers("/accounts/signup", "/accounts/login")
