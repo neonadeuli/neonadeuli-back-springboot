bdf32101db170ad0634e03ef7deda5089ee69d06
eora21 <eora21@naver.com>
2024-03-15 21:09:52 +0900

    feat: 🎸 CustomException 생성자 형태 추가

--- src/main/java/back/neonadeuli/exception/CustomException.java
+++ src/main/java/back/neonadeuli/exception/CustomException.java
@@ -8,12 +8,22 @@ public abstract class CustomException extends RuntimeException {
 
     private final HttpStatus httpStatus;
 
+    protected CustomException(String message) {
+        this(message, HttpStatus.BAD_REQUEST);
+    }
+
+    protected CustomException(String message, Throwable cause) {
+        super(message, cause);
+        this.httpStatus = HttpStatus.BAD_REQUEST;
+    }
+
     protected CustomException(String message, HttpStatus httpStatus) {
         super(message);
         this.httpStatus = httpStatus;
     }
 
-    protected CustomException(String message) {
-        this(message, HttpStatus.BAD_REQUEST);
+    protected CustomException(String message, Throwable cause, HttpStatus httpStatus) {
+        super(message, cause);
+        this.httpStatus = httpStatus;
     }
 }
