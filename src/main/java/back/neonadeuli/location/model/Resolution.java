148be57b8012c0ca1c552b6588ef6bc7f3b0abbe
eora21 <eora21@naver.com>
2024-03-17 10:49:51 +0900

    feat: 🎸 (Resolution) h3Cell을 내부에서 만들어 제공하도록 작성

--- src/main/java/back/neonadeuli/location/model/Resolution.java
+++ src/main/java/back/neonadeuli/location/model/Resolution.java
@@ -1,5 +1,9 @@
 package back.neonadeuli.location.model;
 
+import com.uber.h3core.H3Core;
+import com.uber.h3core.util.LatLng;
+import java.io.IOException;
+import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,5 +13,24 @@ public enum Resolution {
     RES_6(6),
     RES_4(4);
 
+    private static final H3Core H3 = getH3Core();
+
+    private static H3Core getH3Core() {
+        try {
+            return H3Core.newInstance();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
     private final int level;
+
+    public H3Cell calculateH3Cell(int cellSpacingDistance) {
+        return new H3Cell(this, latLng -> latLngToH3Indexes(latLng, cellSpacingDistance));
+    }
+
+    private List<Long> latLngToH3Indexes(LatLng latLng, int cellSpacingDistance) {
+        long cell = H3.latLngToCell(latLng.lat, latLng.lng, this.level);
+        return H3.gridDisk(cell, cellSpacingDistance);
+    }
 }
