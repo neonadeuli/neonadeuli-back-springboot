54196bd2bda7378fa0b4a881f150407dca5432bd
eora21 <eora21@naver.com>
2024-03-17 11:53:51 +0900

    feat: 🎸 (PostService) 글 조회 메서드 작성

--- src/main/java/back/neonadeuli/post/service/PostService.java
+++ src/main/java/back/neonadeuli/post/service/PostService.java
@@ -4,19 +4,29 @@ import back.neonadeuli.account.entity.Account;
 import back.neonadeuli.account.model.authonticate.AccountDetail;
 import back.neonadeuli.account.repository.AccountRepository;
 import back.neonadeuli.location.entity.Location;
+import back.neonadeuli.location.model.GeometryDistance;
+import back.neonadeuli.location.model.H3Cell;
 import back.neonadeuli.location.model.LocationSupplier;
+import back.neonadeuli.location.model.Resolution;
+import back.neonadeuli.location.model.SearchBound;
+import back.neonadeuli.location.model.ZoomLevelSearchBoundConverter;
 import back.neonadeuli.location.repository.LocationRepository;
 import back.neonadeuli.picture.entity.Picture;
 import back.neonadeuli.picture.model.PictureSupplier;
 import back.neonadeuli.picture.model.PictureTemp;
 import back.neonadeuli.picture.repository.PictureRepository;
 import back.neonadeuli.post.dto.request.NewPostRequestDto;
+import back.neonadeuli.post.dto.response.PostResponseDto;
 import back.neonadeuli.post.entity.Post;
 import back.neonadeuli.post.entity.PostPicture;
 import back.neonadeuli.post.repository.PostPictureRepository;
 import back.neonadeuli.post.repository.PostRepository;
+import com.uber.h3core.util.LatLng;
+import jakarta.annotation.PostConstruct;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,7 +41,19 @@ public class PostService {
     private final PostRepository postRepository;
     private final PostPictureRepository postPictureRepository;
     private final PictureRepository pictureRepository;
+    private final ZoomLevelSearchBoundConverter zoomLevelSearchBoundConverter;
     private final LocationRepository locationRepository;
+    private Map<Class<? extends SearchBound>, GetPostsFunction> postResponseFunctions;
+
+    @PostConstruct
+    private void createPostResponseSupplier() {
+        postResponseFunctions = Map.of(
+                GeometryDistance.class, (accountDetail, latLng, geometryRange, pageable) ->
+                        getPostsUsingGeo(accountDetail, latLng, (GeometryDistance) geometryRange, pageable),
+                H3Cell.class, (accountDetail, latLng, h3Cells, pageable) ->
+                        getPostsUsingH3(accountDetail, latLng, (H3Cell) h3Cells, pageable)
+        );
+    }
 
     @Transactional
     public Long uploadNewPost(NewPostRequestDto requestDto, AccountDetail accountDetail) {
@@ -64,5 +86,29 @@ public class PostService {
         pictureTemps.forEach(pictureSupplier::savePicture);
 
         return post.getId();
+    }
+
+    public List<PostResponseDto> getPosts(AccountDetail accountDetail, LatLng latLng, double zoomLevel,
+                                          Pageable pageable) {
+
+        SearchBound searchBound = zoomLevelSearchBoundConverter.getSearchBound(zoomLevel);
+
+        return postResponseFunctions.getOrDefault(searchBound.getClass(), (not, support, search, bound) -> {
+            throw new IllegalStateException();
+        }).getPosts(accountDetail, latLng, searchBound, pageable);
+    }
+
+    private List<PostResponseDto> getPostsUsingGeo(AccountDetail accountDetail, LatLng latLng,
+                                                   GeometryDistance geometryDistance, Pageable pageable) {
+
+        return postRepository.retrievePosts(accountDetail, latLng, geometryDistance, pageable);
+    }
+
+    private List<PostResponseDto> getPostsUsingH3(AccountDetail accountDetail, LatLng latLng, H3Cell h3Cell,
+                                                  Pageable pageable) {
+
+        List<Long> h3Indexes = h3Cell.latLngToH3Indexes().apply(latLng);
+        Resolution resolution = h3Cell.resolution();
+        return postRepository.retrievePosts(accountDetail, resolution, h3Indexes, pageable);
     }
 }
