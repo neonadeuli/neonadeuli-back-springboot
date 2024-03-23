26bd2003cb04ef8c077807f118de9d6de4ba9180
eora21 <eora21@naver.com>
2024-03-23 16:26:44 +0900

    feat: 🎸 (SavePictureFailException) 메시지만 지니는 생성자 추가

--- src/main/java/back/neonadeuli/picture/exception/SavePictureFailException.java
+++ src/main/java/back/neonadeuli/picture/exception/SavePictureFailException.java
@@ -4,8 +4,13 @@ import back.neonadeuli.exception.CustomException;
 import org.springframework.http.HttpStatus;
 
 public class SavePictureFailException extends CustomException {
+    private static final String MESSAGE = "이미지 저장에 실패하였습니다. 다시 시도해주세요.";
+
+    public SavePictureFailException() {
+        super(MESSAGE);
+    }
 
     public SavePictureFailException(Exception e) {
-        super("이미지 저장에 실패하였습니다. 다시 시도해주세요.", e, HttpStatus.INTERNAL_SERVER_ERROR);
+        super(MESSAGE, e, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
