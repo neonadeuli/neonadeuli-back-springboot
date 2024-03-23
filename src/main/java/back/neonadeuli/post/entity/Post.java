1a67eb6fa6ff6580e7a1b128d7e2318093d25ca2
eora21 <eora21@naver.com>
2024-03-23 16:23:57 +0900

    refactor: 💡 (Post) location, postPicture 양방향 및 cascade 설정

--- src/main/java/back/neonadeuli/post/entity/Post.java
+++ src/main/java/back/neonadeuli/post/entity/Post.java
@@ -1,19 +1,28 @@
 package back.neonadeuli.post.entity;
 
+import static jakarta.persistence.CascadeType.DETACH;
+import static jakarta.persistence.CascadeType.MERGE;
+import static jakarta.persistence.CascadeType.PERSIST;
+import static jakarta.persistence.CascadeType.REFRESH;
+
 import back.neonadeuli.account.entity.Account;
 import back.neonadeuli.location.entity.Location;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -47,9 +56,12 @@ public class Post {
     @NotNull
     private String content;
 
-    @OneToOne
+    @OneToOne(cascade = {PERSIST, MERGE, REFRESH, DETACH})
     @JoinColumn(name = "location_id")
     Location location;
+
+    @OneToMany(mappedBy = "post", fetch = FetchType.EAGER, cascade = {PERSIST, MERGE, REFRESH, DETACH})
+    List<PostPicture> postPictures = new ArrayList<>();
 
     public Post(Visibility visibility, Account account, Boolean locationAvailable, String content, Location location) {
         this.visibility = visibility;
