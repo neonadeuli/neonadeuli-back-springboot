544d9e836b4a6fb2ef1c8b8587e9272129240c4d
eora21 <eora21@naver.com>
2024-03-27 13:27:30 +0900

    refactor: 💡 LatLng에 대한 의존성 제거

--- src/main/java/back/neonadeuli/location/model/H3Cell.java
+++ src/main/java/back/neonadeuli/location/model/H3Cell.java
@@ -1,8 +1,4 @@
 package back.neonadeuli.location.model;
 
-import com.uber.h3core.util.LatLng;
-import java.util.List;
-import java.util.function.Function;
-
-public record H3Cell(Resolution resolution, Function<LatLng, List<Long>> latLngToH3Indexes) implements SearchBound {
+public record H3Cell(Resolution resolution, LatLngToH3IndexesFunction latLngToH3Indexes) implements SearchBound {
 }
